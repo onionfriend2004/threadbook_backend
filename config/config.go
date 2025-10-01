0b67a287988a026c0fba35b5fc4aa8fa8d3c6ef9
WonderBro <froloff1830@gmail.com>
2025-10-01 16:24:15 +0300

    THB#10: add config parser template, add viper pkg

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB struct {
+		Host     string `mapstructure:"host"`
+		Port     int    `mapstructure:"port"`
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Name     string `mapstructure:"name"`
+		SSLMode  string `mapstructure:"sslmode"`
+	} `mapstructure:"db"`
+
+	MinIO struct {
+		URL    string `mapstructure:"url"`
+		Bucket string `mapstructure:"bucket"`
+	} `mapstructure:"minio"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
