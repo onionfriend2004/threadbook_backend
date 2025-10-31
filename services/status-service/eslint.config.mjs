e678decd85cea348d26baed4563f03fb87b222fa
WonderBro <froloff1830@gmail.com>
2025-10-31 18:57:05 +0300

    THB#142: add base docs
    fix readme
    
    add full endpoints doc
    
    Update README.md
    
    change img size
    
    Update README.md
    
    Update README.md

--- services/status-service/eslint.config.mjs
+++ services/status-service/eslint.config.mjs
@@ -26,7 +26,7 @@ export default tseslint.config(
   },
   {
     rules: {
-      "linebreak-style": "off",
+      'linebreak-style': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
--- services/status-service/src/main.ts
+++ services/status-service/src/main.ts
@@ -9,11 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({
-      logger: true, // логгер Fastify
+      logger: true,
     }),
   );
 
-  // Важно для вебхуков (если приходят JSON)
+  // JSON
   app.useBodyParser('json');
 
   await app.listen(3000, '0.0.0.0');
