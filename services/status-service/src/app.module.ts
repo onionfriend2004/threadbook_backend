5088ea2850ed75c25005a6874285a14591dda2c5
WonderBro <froloff1830@gmail.com>
2025-10-31 18:57:05 +0300

    THB#142: add base docs
    fix readme
    
    add full endpoints doc
    
    Update README.md
    
    change img size
    
    Update README.md
    
    Update README.md

--- services/status-service/src/app.module.ts
+++ services/status-service/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StatusModule } from './status/status.module';
+import { PostgresModule } from './shared/postgres/postgres.module';
+import { RedisModule } from './shared/redis/redis.module';
 
 @Module({
-  imports: [StatusModule],
+  imports: [StatusModule, PostgresModule, RedisModule],
   controllers: [],
   providers: [],
 })
