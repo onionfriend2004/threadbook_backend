9d60e1433cfbb4eb26586c355de347785d6deeb0
wonderBro <froloff1830@gmail.com>
2026-01-21 13:35:57 +0300

    THB#266: roflofix

--- internal/thread/delivery/dto/update_message_request.go
+++ internal/thread/delivery/dto/update_message_request.go
@@ -1,5 +1,5 @@
 package dto
 
 type UpdateMessageRequest struct {
-	Content string `json:"content" validate:"required,min=1,max=2000"`
+	Content string `json:"content" validate:"required,max=2000"`
 }
